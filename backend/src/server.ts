c5c58a631e1dd80c72d25d57db2bd902eb8cb49f
Chris Tang <tangch3@hotmail.com>
2025-02-15 23:46:11 +0000

    ignore backend node_modules

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -61,6 +61,8 @@ app.put("/update-status/:id", (req, res) => {
   res.status(200).json({ message: "Todo updated" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
